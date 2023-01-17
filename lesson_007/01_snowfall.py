b67a9bd44b98c9e8e69ee135d6e69713cd3834d6
vion <vion@inbox.ru>
2023-01-18 02:23:33 +0300

    lesson_007_home_work

--- lesson_007/01_snowfall.py
+++ lesson_007/01_snowfall.py
@@ -2,6 +2,7 @@
 
 import simple_draw as sd
 
+
 # Шаг 1: Реализовать падение снежинки через класс. Внести в методы:
 #  - создание снежинки с нужными параметрами
 #  - отработку изменений координат
@@ -9,35 +10,70 @@ import simple_draw as sd
 
 
 class Snowflake:
-    pass
+    def __init__(self):
+        self.x = sd.random_number(0, 600)
+        self.y = sd.random_number(0, 600)
+        self.length = sd.random_number(5, 25)
 
-    # TODO здесь ваш код
+    def clear_previous_picture(self):
+        center = sd.get_point(x=self.x, y=self.y)
+        sd.snowflake(center=center, length=self.length, color=sd.background_color)
+
+    def move(self):
+        self.x += sd.random_number(-5, 5)
+        self.y -= sd.random_number(5, 10)
+
+    def draw(self):
+        center = sd.get_point(x=self.x, y=self.y)
+        sd.snowflake(center=center, length=self.length)
+
+    def can_fall(self):
+        if self.y < 0:
+            self.y += 600
+        if self.x > 600:
+            self.x -= 600
 
 
-flake = Snowflake()
+def get_flakes(count):
+    i = 0
+    new_flakes = []
+    while i < N:
+        new_flakes.append(Snowflake())
+        i += 1
+    return new_flakes
 
-while True:
-    flake.clear_previous_picture()
-    flake.move()
-    flake.draw()
-    if not flake.can_fall():
-        break
-    sd.sleep(0.1)
-    if sd.user_want_exit():
-        break
 
-# шаг 2: создать снегопад - список объектов Снежинка в отдельном списке, обработку примерно так:
-# flakes = get_flakes(count=N)  # создать список снежинок
+# def get_fallen_flakes():
+#     if flake.y < 0:
+#         return 1
+
+
+# flake = Snowflake()
+
 # while True:
-#     for flake in flakes:
-#         flake.clear_previous_picture()
-#         flake.move()
-#         flake.draw()
-#     fallen_flakes = get_fallen_flakes()  # подчитать сколько снежинок уже упало
-#     if fallen_flakes:
-#         append_flakes(count=fallen_flakes)  # добавить еще сверху
+#     flake.clear_previous_picture()
+#     flake.move()
+#     flake.draw()
+#     flake.can_fall()
 #     sd.sleep(0.1)
 #     if sd.user_want_exit():
 #         break
+
+# шаг 2: создать снегопад - список объектов Снежинка в отдельном списке, обработку примерно так:
+N = 100
+fallen_flakes = 0
+flakes = get_flakes(count=N)  # создать список снежинок
+while True:
+    for flake in flakes:
+        flake.clear_previous_picture()
+        flake.move()
+        flake.draw()
+        flake.can_fall()
+    # fallen_flakes += get_fallen_flakes()  # подчитать сколько снежинок уже упало
+    # if fallen_flakes:
+    #     append_flakes(count=fallen_flakes)  # добавить еще сверху
+    sd.sleep(0.1)
+    if sd.user_want_exit():
+        break
 
 sd.pause()
--- lesson_007/python_snippets/08_practice.py
+++ lesson_007/python_snippets/08_practice.py
@@ -37,6 +37,11 @@ class Man:
         self.house.money += 10
         self.fullness -= 5
 
+    def cleaning(self):
+        cprint('{} Убрал мусор в доме'.format(self.name), color='blue')
+        self.house.mud = 100
+        self.fullness -= 5
+
     def watch_MTV(self):
         cprint('{} смотрел MTV целый день'.format(self.name), color='green')
         self.fullness -= 10
@@ -51,22 +56,36 @@ class Man:
             cprint('{} деньги кончились!'.format(self.name), color='red')
             self.work()
 
+    def shopping_pets(self):
+        if self.house.money > 0:
+            cprint('{} сходил в магазин за едой для животных'.format(self.name), color='magenta')
+            # self.house.money -= 50
+            self.house.food_pet = self.house.money
+            self.house.money = 0
+        else:
+            cprint('{} деньги кончились!'.format(self.name), color='red')
+            self.work()
+
     def go_to_the_house(self, house):
         self.house = house
         self.fullness -= 10
         cprint('{} Вьехал в дом'.format(self.name), color='cyan')
 
     def act(self):
-        if self.fullness <= 0:
+        if self.fullness < 0:
             cprint('{} умер...'.format(self.name), color='red')
             return
         dice = randint(1, 6)
         if self.fullness < 20:
             self.eat()
+        elif self.house.food_pet < 5:
+            self.shopping_pets()
         elif self.house.food < 10:
             self.shopping()
         elif self.house.money < 50:
             self.work()
+        elif self.house.mud <= 0:
+            self.cleaning()
         elif dice == 1:
             self.work()
         elif dice == 2:
@@ -75,15 +94,72 @@ class Man:
             self.watch_MTV()
 
 
+class Cat:
+
+    def __init__(self, name):
+        self.name = name
+        self.fullness = 20
+        self.house = None
+
+    def __str__(self):
+        return 'Кот - {}, сытость {}'.format(
+            self.name, self.fullness)
+
+    def eat(self):
+        if self.house.food_pet >= 0:
+            cprint('Кот {} поел'.format(self.name), color='yellow')
+            self.fullness += 5
+            self.house.food_pet -= 5
+        else:
+            cprint('Кот {} нет еды'.format(self.name), color='red')
+            self.sleep()
+
+    def sleep(self):
+        cprint('Кто {} спал целый день'.format(self.name), color='green')
+        self.fullness -= 5
+
+    def play(self):
+        cprint('Кто {} играл целый день'.format(self.name), color='green')
+        self.fullness -= 5
+
+    def tear_wallpaper(self):
+        cprint('Кто {} Дерет обои'.format(self.name), color='green')
+        self.fullness -= 5
+        self.house.mud -= 10
+
+    def go_to_the_house(self, house):
+        self.house = house
+        self.fullness -= 5
+        cprint('Кот {} Вьехал в дом'.format(self.name), color='cyan')
+
+    def act(self):
+        if self.fullness < 0:
+            cprint('Кот {} умер...'.format(self.name), color='red')
+            return
+        dice = randint(1, 6)
+        if self.fullness < 5:
+            self.eat()
+        elif dice == 1:
+            self.play()
+        elif dice == 2:
+            self.eat()
+        elif dice == 3:
+            self.tear_wallpaper()
+        else:
+            self.sleep()
+
+
 class House:
 
     def __init__(self):
         self.food = 50
+        self.food_pet = 0
         self.money = 0
+        self.mud = 100
 
     def __str__(self):
-        return 'В доме еды осталось {}, денег осталось {}'.format(
-            self.food, self.money)
+        return 'В доме еды осталось {}, еды для питомцев {}, денег осталось {}, чистота {}%'.format(
+            self.food, self.food_pet, self.money, self.mud)
 
 
 citizens = [
@@ -92,18 +168,30 @@ citizens = [
     Man(name='Кенни'),
 ]
 
+pets = [
+    Cat(name='Барсик'),
+    Cat(name='Персик')
+]
+
 
 my_sweet_home = House()
 for citisen in citizens:
     citisen.go_to_the_house(house=my_sweet_home)
+for pet in pets:
+    pet.go_to_the_house(house=my_sweet_home)
+
 
 for day in range(1, 366):
     cprint('================ день {} =================='.format(day), color='yellow')
     for citisen in citizens:
         citisen.act()
+    for pet in pets:
+        pet.act()
     print('--- в конце дня ---')
     for citisen in citizens:
         print(citisen)
+    for pet in pets:
+        print(pet)
     print(my_sweet_home)
 
 # Создадим двух людей, живущих в одном доме - Бивиса и Батхеда
