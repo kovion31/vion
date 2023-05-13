dcd5ebf25206e4d591f8018d753f2dba4eb6218d
vion <vion@inbox.ru>
2023-05-13 23:32:06 +0300

    lesson_012

---
+++ lesson_011/python_snippets/prode.py
@@ -0,0 +1,31 @@
+
+animal = 'мишка'
+
+
+def stutter(text):
+    return text[:2] + '-' + text
+
+print(stutter(animal))
+
+
+def stutter_factory(n):
+
+    def stutter(text):
+        return (text[:2] + '-') * n + text
+
+    return stutter
+
+stutter_2 = stutter_factory(n=2)
+print(stutter_2(animal))
+stutter_3 = stutter_factory(n=3)
+print(stutter_3(animal))
+
+
+stutters = [stutter_factory(n=n) for n in range(1, 4)]
+print(stutters)
+result = [func(animal) for func in stutters]
+print(result)
+
+animals = ['зайка', 'мишка', 'бегемотик']
+mesh = [func(animal) for animal in animals for func in stutters]
+print(mesh)
